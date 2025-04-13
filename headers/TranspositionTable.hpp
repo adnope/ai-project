f089b65ae2424e0c99a567b1a75371ea3cd0a00b
adnope <anhduyxh7@gmail.com>
2025-04-13 17:17:42 +0700

    Added linear probing to transposition table & renamed tests

--- headers/TranspositionTable.hpp
+++ headers/TranspositionTable.hpp
@@ -10,7 +10,7 @@ class TranspositionTable
 private:
 	struct Entry
 	{
-		uint64_t key : 56;
+		uint64_t key;
 		uint8_t val;
 	};
 
@@ -34,47 +34,29 @@ public:
 
 	void Put(uint64_t key, uint8_t val)
 	{
-		assert(key < (1LL << 56));
 		unsigned int i = index(key);
+		while (T[i].key != 0 && T[i].key != key)
+		{
+			i = (i + 1) % T.size();
+		}
 		T[i].key = key;
 		T[i].val = val;
 	}
 
 	uint8_t Get(uint64_t key) const
 	{
-		assert(key < (1LL << 56));
 		unsigned int i = index(key);
-		if (T[i].key == key)
-			return T[i].val;
-		else
-			return 0;
-	}
-
-	using partial_key_t = uint64_t;
-	using value_t = uint8_t;
-
-	const uint64_t *GetKeys() const
-	{
-		return reinterpret_cast<const uint64_t *>(&T[0]);
-	}
-
-	const uint8_t *GetValues() const
-	{
-		return reinterpret_cast<const uint8_t *>(&T[0].val);
+		while (T[i].key != 0)
+		{
+			if (T[i].key == key)
+				return T[i].val;
+			i = (i + 1) % T.size();
+		}
+		return 0;
 	}
 
 	size_t GetSize() const
 	{
 		return T.size();
-	}
-
-	int GetKeySize() const
-	{
-		return sizeof(partial_key_t);
-	}
-
-	int GetValueSize() const
-	{
-		return sizeof(value_t);
 	}
 };
